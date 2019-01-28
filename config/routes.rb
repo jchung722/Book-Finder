f8a4441555a4c11d950c621659cd53ed5f980232
jchung722 <jchung722@gmail.com>
2019-01-28 10:59:26 -0600

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
   root to: 'books#index'
+
+  get '/books/:id', to: 'books#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
