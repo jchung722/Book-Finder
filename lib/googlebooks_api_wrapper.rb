30385933b0cfff7b52368b467bb7f39e360091f9
jchung722 <jchung722@gmail.com>
2019-01-28 11:41:35 -0600

    add search to api wrapper

--- lib/googlebooks_api_wrapper.rb
+++ lib/googlebooks_api_wrapper.rb
@@ -4,4 +4,27 @@ require 'httparty'
 class BooksApiWrapper
   BASE_URL = "https://www.googleapis.com/books/v1/volumes"
   TOKEN = ENV["KEY"]
+
+  def self.search(query)
+    url = BASE_URL + "&q=#{query}"
+    response = HTTParty.get(url)
+    books = []
+
+    if response["items"]
+      response["items"].each do |item|
+        title = item["volumeInfo"]["title"]
+        author = item["volumeInfo"]["authors"]
+        publisher = item["volumeInfo"]["publisher"]
+        image = item["volumeInfo"]["imageLinks"]["thumbnail"]
+        link = item["volumeInfo"]["infoLink"]
+
+        books << Book.new(title, author, publisher, image, link)
+    end
+    else
+      return nil
+    end
+
+    return books
+  end
+
 end
