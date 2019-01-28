430ccc2359c1c3c9f7bcab6595e47d7020d2ce2c
jchung722 <jchung722@gmail.com>
2019-01-28 12:07:34 -0600

    add data to controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
   def index
+    if params[:q].present?
+      @data = paginate BooksApiWrapper.search(params[:q]), per_page: 10
+    end
   end
 
   def show
