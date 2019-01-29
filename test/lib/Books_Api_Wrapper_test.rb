08612f4f503be0fbde7546fa6d674e158010ad39
jchung722 <jchung722@gmail.com>
2019-01-28 20:25:06 -0600

    add api wrapper test

---
+++ test/lib/Books_Api_Wrapper_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+describe BooksApiWrapper do
+
+  it "Can Retrieve a list of books based on query" do
+    VCR.use_cassette("books") do
+      books = BooksApiWrapper.search("Harry","")
+
+      assert books.is_a? Array
+      assert books.length == 10
+      books.each do |book|
+        assert book.is_a? Book
+      end
+    end
+  end
+
+  it "Retrieves nil when given the wrong search terms with no matching items" do
+    VCR.use_cassette("bad-search-terms") do
+      books = BooksApiWrapper.search("dlaksdjfas","inauthor:")
+
+      assert books == nil
+    end
+  end
+end
