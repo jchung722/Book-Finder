679c83768118f7736d281e8901bc2944aecdf91d
jchung722 <jchung722@gmail.com>
2019-02-14 09:01:39 -0600

    disable unused rake middleware

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,32 @@ module BookFinder
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    #allow access to lib file for API consumption & Book object
     config.eager_load_paths << Rails.root.join('lib')
-    # config/application.rb
+
+    #add config for minitest
     config.generators do |g|
       g.test_framework :minitest, spec: true
     end
+
+    #Disable unused Rack middleware
+    config.eager_load = false
+    config.middleware.delete Rack::Sendfile
+    config.middleware.delete ActiveSupport::Cache::Strategy::LocalCache::Middleware
+    config.middleware.delete Rack::Runtime
+    config.middleware.delete Rack::MethodOverride
+    config.middleware.delete ActionDispatch::RequestId
+    config.middleware.delete Rails::Rack::Logger
+    config.middleware.delete ActionDispatch::ShowExceptions
+    config.middleware.delete ActionDispatch::DebugExceptions
+    config.middleware.delete ActionDispatch::Reloader
+    config.middleware.delete ActionDispatch::Callbacks
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
+    config.middleware.delete ActionDispatch::Flash
+    config.middleware.delete Rack::Head
+    config.middleware.delete Rack::ConditionalGet
+    config.middleware.delete Rack::ETag
   end
 end
