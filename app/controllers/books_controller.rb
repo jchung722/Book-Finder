b25dd62cb6ba6464fad30b5a3746d77bd2c244cc
jchung722 <jchung722@gmail.com>
2019-02-14 04:17:29 -0600

    remove more boilerplate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,4 @@
-class BooksController < ApplicationController
+class BooksController < ActionController::Base
   def index
     if params[:q].present?
       @data = BooksApiWrapper.search(params[:q], params[:search])
