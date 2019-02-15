64374a2e0e1dec4b782f3ee6e056a91edc4ba793
jchung722 <jchung722@gmail.com>
2019-02-14 23:08:07 -0600

    update lib tests

--- test/lib/Books_Api_Wrapper_test.rb
+++ test/lib/Books_Api_Wrapper_test.rb
@@ -2,9 +2,25 @@ require 'test_helper'
 
 describe BooksApiWrapper do
 
+  it "must initialize with an api class object" do
+    wrapper = BooksApiWrapper.new(GoogleBooks)
+  end
+
+  it "must correctly assign api info to wrapper" do
+    wrapper = BooksApiWrapper.new(GoogleBooks)
+    assert wrapper.api == GoogleBooks
+  end
+
+  it "must fail when initialized without an argument" do
+    assert_raises ArgumentError do
+      wrapper = BooksApiWrapper.new()
+    end
+  end
+
   it "Can Retrieve a list of books based on query" do
     VCR.use_cassette("books") do
-      books = BooksApiWrapper.search("Harry","")
+      wrapper = BooksApiWrapper.new(GoogleBooks)
+      books = wrapper.search("Harry","")
 
       assert books.is_a? Array
       assert books.length == 10
@@ -14,11 +30,51 @@ describe BooksApiWrapper do
     end
   end
 
+  it "Can Retrieve a list of books based on a query and search term" do
+    VCR.use_cassette("books-with-search-terms") do
+      wrapper = BooksApiWrapper.new(GoogleBooks)
+      books = wrapper.search("Harry","inauthor:")
+
+      assert books.is_a? Array
+      assert books.length == 10
+      books.each do |book|
+        assert book.is_a? Book
+      end
+    end
+  end
+
+  it "Must prioritize book search with query specific to search term" do
+    VCR.use_cassette("specific-term-search") do
+      wrapper = BooksApiWrapper.new(GoogleBooks)
+      books = wrapper.search("Eric","inauthor:")
+
+
+      assert books[0].authors[0].include? "Eric"
+    end
+  end
+
+  it "Retrieves nil when given no query" do
+    VCR.use_cassette("no-query-term") do
+      wrapper = BooksApiWrapper.new(GoogleBooks)
+      books = wrapper.search(nil, nil)
+
+      assert_nil books
+    end
+  end
+
   it "Retrieves nil when given the wrong search terms with no matching items" do
     VCR.use_cassette("bad-search-terms") do
-      books = BooksApiWrapper.search("dlaksdjfas","inauthor:")
+      wrapper = BooksApiWrapper.new(GoogleBooks)
+      books = wrapper.search("dlaksdjfas","inauthor:")
 
       assert books == nil
+    end
+  end
+
+  it "search method error when given wrong number of arguments" do
+    assert_raises ArgumentError do
+      wrapper = BooksApiWrapper.new(GoogleBooks)
+      books = wrapper.search("inauthor:")
     end
   end
 end
--- test/lib/Google_Books_test.rb
+++ test/lib/Google_Books_test.rb
@@ -14,11 +14,47 @@ describe GoogleBooks do
     end
   end
 
-  it "Retrieves nil when given the wrong search terms with no matching items" do
-    VCR.use_cassette("bad-search-terms") do
-      books = BooksApiWrapper.search("dlaksdjfas","inauthor:")
+  it "Can Retrieve a list of books based on a query and search term" do
+    VCR.use_cassette("books-with-search-terms") do
+      books = GoogleBooks.search("Harry","inauthor:")
 
-      assert books == nil
+      assert books.is_a? Array
+      assert books.length == 10
+      books.each do |book|
+        assert book.is_a? Book
+      end
     end
   end
+
+  it "Must prioritize book search with query specific to search term" do
+    VCR.use_cassette("specific-term-search") do
+      books = GoogleBooks.search("Eric","inauthor:")
+
+      assert books[0].authors[0].include? "Eric"
+    end
+  end
+
+  it "Retrieves nil when given no query" do
+    VCR.use_cassette("no-query-term") do
+      books = GoogleBooks.search(nil,nil)
+
+      assert_nil books
+    end
+  end
+
+  it "Retrieves nil when given the wrong search terms with no matching items" do
+    VCR.use_cassette("bad-search-terms") do
+      books = GoogleBooks.search("dlaksdjfas","inauthor:")
+
+      assert_nil books
+
+    end
+  end
+
+  it "Search method raises error when given wrong number of arguments" do
+    assert_raises ArgumentError do
+      books = GoogleBooks.search("argument")
+    end
+  end
+
 end
